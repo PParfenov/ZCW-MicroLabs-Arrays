6d36ac3dd7226ad2a9a0935d932603559d38a581
pavel <Pavelparfenov112@gmail.com>
2017-11-09 16:58:52 -0500

    "Done"

--- src/main/java/io/zipcoder/microlabs/arrays/ArrayParty.java
+++ src/main/java/io/zipcoder/microlabs/arrays/ArrayParty.java
@@ -1,26 +1,72 @@
 package io.zipcoder.microlabs.arrays;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class ArrayParty {
 
     public String printArray(String[] inputArray){
-        return "";
+        String output = "*** Output ***";
+        for (String word :
+                inputArray) {
+            output += "\n" + word;
+        }
+        return output;
     }
 
     public String lastElement(String[] inputArray){
-        return "";
+        String output = "*** Output ***";
+        output+="\n"+inputArray[inputArray.length-1];
+        return output;
     }
 
     //TODO Define the method lastButOne
+    public String lastButOne(String[] inputArray){
+        String output = "*** Output ***";
+        output+="\n"+inputArray[inputArray.length-2];
+        return output;
+    }
 
     //TODO Define the method reverse
+    public String reverse(String[] inputArray){
+        Collections.reverse(Arrays.asList(inputArray));
+        String output = "*** Output ***";
+        for (String word :
+                inputArray) {
+            output += "\n" + word;
+        }
+        return output;
+    }
 
     //TODO Define the method isPalindrome
+    public boolean isPalindrome(String[] inputArray) {
+        return printArray(inputArray).equals(reverse(inputArray));
+    }
 
     //TODO Define the method compress
+    public String compress(int[] inputArray){
+        String outputString = "*** Output ***\n"+inputArray[0];
+        for (int i = 1; i < inputArray.length; i++){
+            if (inputArray[i] != inputArray[i-1]){
+                outputString += "\n"+inputArray[i];
+            }
+        }
+        return outputString;
+    }
 
     //TODO Define the method pack
-
-
-
+    public String pack(char[] letters){
+        String outputString = "*** Output ***\n"+letters[0];
+        for (int i = 1; i < letters.length; i++){
+            if(letters[i] == letters[i-1]){
+                outputString += letters[i];
+            } else {
+                outputString += ", "+letters[i];
+            }
+        }
+        return outputString;
+    }
 }
--- src/test/java/io/zipcoder/microlabs/arrays/ArrayPartyTest.java
+++ src/test/java/io/zipcoder/microlabs/arrays/ArrayPartyTest.java
@@ -28,21 +28,94 @@ public class ArrayPartyTest {
     @Test
     public void lastElementTest(){
         //: Given
+        String[] breakfast = {"Sausage", "Eggs", "Beans", "Bacon", "Tomatoes", "Mushrooms"};
+        ArrayParty arrayParty = new ArrayParty();
+        String expected = "*** Output ***\n" +
+                "Mushrooms";
 
         //: When
+        String actual = arrayParty.lastElement(breakfast);
 
         //: Then
+        Assert.assertEquals("The two string outputs are equal", expected, actual);
     }
 
     //TODO Define the method lastButOneTest
+    @Test
+    public void lastButOneTest(){
+        //: Given
+        String[] breakfast = {"Sausage", "Eggs", "Beans", "Bacon", "Tomatoes", "Mushrooms"};
+        ArrayParty arrayParty = new ArrayParty();
+        String expected = "*** Output ***\n" +
+                "Tomatoes";
+
+        //: When
+        String actual = arrayParty.lastButOne(breakfast);
+
+        //: Then
+        Assert.assertEquals("The two string outputs are equal", expected, actual);
+    }
 
     //TODO Define the method reverseTest
+    @Test
+    public void reverseTest(){
+        //: Given
+        String[] breakfast = {"Sausage", "Eggs", "Beans", "Bacon", "Tomatoes", "Mushrooms"};
+        ArrayParty arrayParty = new ArrayParty();
+        String expected = "*** Output ***\n" +
+                "Mushrooms\n" +
+                "Tomatoes\n" +
+                "Bacon\n" +
+                "Beans\n" +
+                "Eggs\n" +
+                "Sausage";
+
+        //: When
+        String actual = arrayParty.reverse(breakfast);
+
+        //: Then
+        Assert.assertEquals("The two string outputs are equal", expected, actual);
+    }
 
     //TODO Define the method isPalindromeTest
+    @Test
+    public void isPalindromeTest(){
+        ArrayParty arrayParty = new ArrayParty();
+        String[] palindromic = {"Sausage", "Eggs", "Beans", "Beans", "Eggs", "Sausage"};
+        String[] breakfast = {"Sausage", "Eggs", "Beans", "Bacon", "Tomatoes", "Mushrooms"};
+
+        Assert.assertTrue(arrayParty.isPalindrome(palindromic));
+        Assert.assertFalse(arrayParty.isPalindrome(breakfast));
+    }
 
     //TODO Define the method compressTest
+    @Test
+    public void compressTest(){
+        ArrayParty arrayParty = new ArrayParty();
+        int[] nums = {1,1,3,3,3,2,2,2,1,1,1,1,4,4,4,4};
+
+        String expected = "*** Output ***\n" +
+                "1\n" +
+                "3\n" +
+                "2\n" +
+                "1\n" +
+                "4";
+        String actual = arrayParty.compress(nums);
+
+        Assert.assertEquals(expected, actual);
+    }
 
     //TODO Define the method packTest
+    @Test
+    public void packTest(){
+        ArrayParty arrayParty = new ArrayParty();
+        char[] letters = {'a','a','a','a','b','c','c','a','a','d','e','e','e','e'};
 
+        String expected = "*** Output ***\n" +
+                "aaaa, b, cc, aa, d, eeee";
+        String actual = arrayParty.pack(letters);
+
+        Assert.assertEquals(expected, actual);
+    }
 
 }
